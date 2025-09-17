c252b72c36ed24778890b97cb7811c0d9a6a7db3
ManulParihar <manul.singh.parihar@gmail.com>
2025-09-17 15:31:38 +0530

    Added _deployFunds which implements borrowing and lending logic

--- src/contracts/YieldLooping.sol
+++ src/contracts/YieldLooping.sol
@@ -1,6 +1,71 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.30; 
+pragma solidity ^0.8.18;
 
-contract YieldLooping {
+import {BaseStrategy} from "tokenized-strategy/BaseStrategy.sol";
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {IAavePool} from "../interface/IaavePool.sol";
 
+import {Errors} from "./Errors.sol";
+
+/// @notice Aave Looping Strategy for wstETH/wETH pair
+contract YieldLooping is BaseStrategy {
+
+    /// @notice ERC20 token address for wstETH
+    /// @dev Lending token
+    IERC20 public wstETH;
+
+    /// @notice ERC20 token address for wETH
+    /// @dev Borrowing token
+    IERC20 public wETH;
+
+    /// @notice Aave pool address
+    IAavePool public aavePool;
+
+    constructor(
+        address _wstETH,
+        address _wETH,
+        address _aavePool
+    ) BaseStrategy(_wETH, "ynLoopWstETH") {
+        require(_wstETH != address(0), Errors.ZeroAddress());
+        require(_wETH != address(0), Errors.ZeroAddress());
+        require(_aavePool != address(0), Errors.ZeroAddress());
+
+        wstETH = IERC20(_wstETH);
+        wETH = IERC20(_wETH);
+        aavePool = IAavePool(_aavePool);
+
+        // approve spending of wstETH and wETH
+        wstETH.approve(address(aavePool), type(uint256).max);
+        wETH.approve(address(aavePool), type(uint256).max);
+    }
+
+    function _deployFunds(uint256 _amount) internal override returns (uint256 receivedAmount) {
+        require(_amount > 0, Errors.ZeroAmount());
+
+        // Approve spending of wstETH by this vault
+        wstETH.approve(address(aavePool), _amount);
+        // Send wstETH to Aave
+        aavePool.supply(address(wstETH), _amount, address(this), 0);
+
+        uint256 borrowAmount = _calculateBorrow(_amount);
+        // interestRateMode is set to 1 (for stable rate of borrowing)
+        aavePool.borrow(address(wETH), borrowAmount, 1, 0, address(this));
+
+        // Swap wETH to wstETH for next loop
+        receivedAmount = _swap(borrowAmount);
+    }
+
+    /// @dev This function calculates the borrow amount based on hardcoded LTV (Loan-To-Value) of 70%
+    /// Amount of wETH to be borrowed/received after lending/supplying wstETH
+    function _calculateBorrow(uint256 _amount) internal view returns (uint256) {
+        return (_amount * 70) / 100;
+    }
+
+    /// @dev This function swaps wETH to wstETH
+    /// Swaps at 1:1 for simpicity
+    function _swap(
+        uint256 _amount
+    ) internal returns (uint256) {
+        return _amount;
+    } 
 }
--- src/interface/IAavePool.sol
+++ src/interface/IAavePool.sol
@@ -1,5 +1,123 @@
 // SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.18;
 
-contract IAavePool {
+/// @dev this interface contains select functions
+interface IAavePool {
+  /**
+   * @notice Supplies an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+   * - E.g. User supplies 100 USDC and gets in return 100 aUSDC
+   * @param asset The address of the underlying asset to supply
+   * @param amount The amount to be supplied
+   * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+   *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+   *   is a different wallet
+   * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+   *   0 if the action is executed directly by the user, without any middle-man
+   */
+  function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
 
+  /**
+   * @notice Withdraws an `amount` of underlying asset from the reserve, burning the equivalent aTokens owned
+   * E.g. User has 100 aUSDC, calls withdraw() and receives 100 USDC, burning the 100 aUSDC
+   * @param asset The address of the underlying asset to withdraw
+   * @param amount The underlying amount to be withdrawn
+   *   - Send the value type(uint256).max in order to withdraw the whole aToken balance
+   * @param to The address that will receive the underlying, same as msg.sender if the user
+   *   wants to receive it on his own wallet, or a different address if the beneficiary is a
+   *   different wallet
+   * @return The final amount withdrawn
+   */
+  function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+
+  /**
+   * @notice Allows users to borrow a specific `amount` of the reserve underlying asset, provided that the borrower
+   * already supplied enough collateral, or he was given enough allowance by a credit delegator on the VariableDebtToken
+   * - E.g. User borrows 100 USDC passing as `onBehalfOf` his own address, receiving the 100 USDC in his wallet
+   *   and 100 variable debt tokens
+   * @param asset The address of the underlying asset to borrow
+   * @param amount The amount to be borrowed
+   * @param interestRateMode 2 for Variable, 1 is deprecated on v3.2.0
+   * @param referralCode The code used to register the integrator originating the operation, for potential rewards.
+   *   0 if the action is executed directly by the user, without any middle-man
+   * @param onBehalfOf The address of the user who will receive the debt. Should be the address of the borrower itself
+   * calling the function if he wants to borrow against his own collateral, or the address of the credit delegator
+   * if he has been given credit delegation allowance
+   */
+  function borrow(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    uint16 referralCode,
+    address onBehalfOf
+  ) external;
+
+  /**
+   * @notice Repays a borrowed `amount` on a specific reserve, burning the equivalent debt tokens owned
+   * - E.g. User repays 100 USDC, burning 100 variable debt tokens of the `onBehalfOf` address
+   * @param asset The address of the borrowed underlying asset previously borrowed
+   * @param amount The amount to repay
+   * - Send the value type(uint256).max in order to repay the whole debt for `asset` on the specific `debtMode`
+   * @param interestRateMode 2 for Variable, 1 is deprecated on v3.2.0
+   * @param onBehalfOf The address of the user who will get his debt reduced/removed. Should be the address of the
+   * user calling the function if he wants to reduce/remove his own debt, or the address of any other
+   * other borrower whose debt should be removed
+   * @return The final amount repaid
+   */
+  function repay(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    address onBehalfOf
+  ) external returns (uint256);
+
+  /**
+   * @notice Allows smartcontracts to access the liquidity of the pool within one transaction,
+   * as long as the amount taken plus a fee is returned.
+   * @dev IMPORTANT There are security concerns for developers of flashloan receiver contracts that must be kept
+   * into consideration. For further details please visit https://docs.aave.com/developers/
+   * @param receiverAddress The address of the contract receiving the funds, implementing IFlashLoanReceiver interface
+   * @param assets The addresses of the assets being flash-borrowed
+   * @param amounts The amounts of the assets being flash-borrowed
+   * @param interestRateModes Types of the debt to open if the flash loan is not returned:
+   *   0 -> Don't open any debt, just revert if funds can't be transferred from the receiver
+   *   1 -> Deprecated on v3.2.0
+   *   2 -> Open debt at variable rate for the value of the amount flash-borrowed to the `onBehalfOf` address
+   * @param onBehalfOf The address  that will receive the debt in the case of using 2 on `modes`
+   * @param params Variadic packed params to pass to the receiver as extra information
+   * @param referralCode The code used to register the integrator originating the operation, for potential rewards.
+   *   0 if the action is executed directly by the user, without any middle-man
+   */
+  function flashLoan(
+    address receiverAddress,
+    address[] calldata assets,
+    uint256[] calldata amounts,
+    uint256[] calldata interestRateModes,
+    address onBehalfOf,
+    bytes calldata params,
+    uint16 referralCode
+  ) external;
+
+  /**
+   * @notice Returns the user account data across all the reserves
+   * @param user The address of the user
+   * @return totalCollateralBase The total collateral of the user in the base currency used by the price feed
+   * @return totalDebtBase The total debt of the user in the base currency used by the price feed
+   * @return availableBorrowsBase The borrowing power left of the user in the base currency used by the price feed
+   * @return currentLiquidationThreshold The liquidation threshold of the user
+   * @return ltv The loan to value of The user
+   * @return healthFactor The current health factor of the user
+   */
+  function getUserAccountData(
+    address user
+  )
+    external
+    view
+    returns (
+      uint256 totalCollateralBase,
+      uint256 totalDebtBase,
+      uint256 availableBorrowsBase,
+      uint256 currentLiquidationThreshold,
+      uint256 ltv,
+      uint256 healthFactor
+    );
 }
